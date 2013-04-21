ce80a7afe2eb91a2490a9d33e481f86ca7fe2720
Richard Priestley <r.priestley@comanduk.co.uk>
2013-04-21 15:59:45 +0100

    added end2end tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -8,38 +8,29 @@ describe('my app', function() {
     browser().navigateTo('../../app/index.html');
   });
 
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
+  it('should automatically redirect to /home when location hash/fragment is empty', function() {
+    expect(browser().location().url()).toBe("/home");
   });
 
-
-  describe('view1', function() {
+  describe('home', function() {
 
     beforeEach(function() {
-      browser().navigateTo('#/view1');
+      browser().navigateTo('#/home');
     });
 
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
+    it('should render home when user navigates to /home', function() {
+      expect(element('[ng-view] p:first').text()).toMatch(/partial for view 1/);
     });
-
   });
 
-
-  describe('view2', function() {
+  describe('slickgrid', function() {
 
     beforeEach(function() {
-      browser().navigateTo('#/view2');
+      browser().navigateTo('#/slickgrid');
     });
 
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
+    it('should render slickgrid when user navigates to /slickgrid', function() {
+      expect(element('#myGrid', 'grid div').count()).toEqual(1);
     });
-
   });
 });
