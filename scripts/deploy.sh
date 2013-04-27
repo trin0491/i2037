40242e16aa446b63f9fe6cf3665d575dd5756fd9
Richard Priestley <r.priestley@comanduk.co.uk>
2013-04-27 19:24:42 +0100

    local deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #BUILD_HOME=/var/lib/jenkins/jobs/i2037/lastStable/archive
 BUILD_HOME=`dirname $0`/..
-rsync -avz --delete $BUILD_HOME/app/ merlin:/srv/www/i2037/htdocs
+rsync -avz --delete $BUILD_HOME/app/ /srv/www/i2037/htdocs
