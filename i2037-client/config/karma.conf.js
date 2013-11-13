b99480268b1b9bd4ced6b6b58dd87e5cb04b42fa
trin0491 <richard@peregrine.local>
2013-11-13 21:43:11 +0000

    fixed build path of unit tests

--- i2037-client/config/karma.conf.js
+++ i2037-client/config/karma.conf.js
@@ -3,11 +3,10 @@ module.exports = function(config){
     basePath : '../',
 
     files : [
-      'app/lib/jquery/jquery-1.10.2.min.js',
-      'app/lib/angular/angular.js',
-      'app/lib/angular/angular-resource.js',
-      'app/lib/angular/angular-route.js',      
-      'test/lib/angular/angular-mocks.js',
+      'build/dev/js/jquery.js',
+      'build/dev/js/angular.js',
+      'build/dev/js/bootstrap.js',      
+      'bower_components/angular-mocks/angular-mocks.js',
       'https://maps.googleapis.com/maps/api/js?key=AIzaSyD1XkZF2vEwPUZeFfadDMe76ozKhrk-mXE&sensor=false',
     //  'app/js/**/*.js',
       'build/dev/js/dev/i2037-client.js',
