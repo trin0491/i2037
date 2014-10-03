74b49f13d81f3c1df1186386cb4d4704abacba84
trin0491 <richard@peregrine.local>
2014-10-03 15:00:04 +0300

    hack for timezone issue

--- i2037-webapp/src/main/java/net/i2037/flickr/FlickrPhoto.java
+++ i2037-webapp/src/main/java/net/i2037/flickr/FlickrPhoto.java
@@ -1,5 +1,6 @@
 package net.i2037.flickr;
 
+import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
@@ -7,7 +8,10 @@ public final class FlickrPhoto {
 
 	public static final String ID = "id";
 	public static final String DATE_TAKEN = "datetaken";
-	public static final DateTimeFormatter TIME_FORMAT = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
-	
-	private FlickrPhoto() {}
+	public static final DateTimeFormatter TIME_FORMAT = DateTimeFormat
+			.forPattern("yyyy-MM-dd HH:mm:ss").withZone(
+					DateTimeZone.forOffsetHours(3));
+
+	private FlickrPhoto() {
+	}
 }
