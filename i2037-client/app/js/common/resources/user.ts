ccb3b12c9f223fbe1ee41f203c805475cbbeafc7
Richard Priestley <r.priestley@comanduk.co.uk>
2015-03-10 23:09:34 +0000

    converted cage and user to ts

---
+++ i2037-client/app/js/common/resources/user.ts
@@ -0,0 +1,80 @@
+module i2037.resources {
+
+  class User {
+    static get(params):any {
+
+    }
+
+    constructor(data:any) {
+      angular.extend(this, data);
+    }
+  }
+
+  angular.module('i2037.resources.user', ['i2037.services'])
+
+    .factory('User', ['$http', 'pathFinder', function ($http:ng.IHttpService, pathFinder:services.PathFinder) {
+
+      var url:string = pathFinder.get('svc/user');
+
+      function getId(user) {
+        return user.id;
+      }
+
+      var User:any = function (data) {
+        angular.extend(this, data);
+      };
+
+      User.get = function (params) {
+        return $http.get(url).then(function (response) {
+          return new User(response.data);
+        });
+      };
+
+      User.save = function (data) {
+        return $http.post(url, data).then(function (response) {
+          return new User(data);
+        });
+      };
+
+      User.update = function (data) {
+        return $http.post(url + '/' + data.$id(), data).then(function (response) {
+          return data;
+        });
+      };
+
+      User.prototype.$id = function () {
+        return getId(this);
+      };
+
+      User.prototype.$save = function () {
+        return User.save(this);
+      };
+
+      User.prototype.$update = function () {
+        return User.update(this);
+      };
+
+      User.login = function (userName, password) {
+        var loginParams = jQuery.param({
+          j_username: userName,
+          j_password: password
+        });
+        return $http.post(pathFinder.get('j_spring_security_check'), loginParams, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }).then(function (response) {
+          return new User(response.data);
+        });
+      };
+
+      User.logout = function () {
+        return $http.get(pathFinder.get('j_spring_security_logout')).then(function () {
+          return null;
+        });
+      };
+
+      return User;
+    }])
+  ;
+}
