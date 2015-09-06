28bc5d1bba0366eb7d2afd2499ca38156ee9f245
Richard Priestley <r.priestley@comanduk.co.uk>
2015-09-06 21:00:11 +0100

    reformatted

--- i2037-mobile/app/js/app.ts
+++ i2037-mobile/app/js/app.ts
@@ -7,86 +7,92 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 angular.module('i2037-mobile', [
-    'ionic',
-    'i2037.journal'
-    ])
+  'ionic',
+  'i2037.journal'
+])
 
-    .run(function($ionicPlatform) {
-        $ionicPlatform.ready(function() {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
-            if (window.StatusBar) {
-                // org.apache.cordova.statusbar required
-                window.StatusBar.styleDefault();
-            }
-        });
-    })
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        window.StatusBar.styleDefault();
+      }
+    });
+  })
 
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-            .state('app', {
-                url: "/app",
-                abstract: true,
-                templateUrl: "templates/menu.html",
-                controller: 'MenuCtrl'
-            })
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'MenuCtrl'
+      })
 
-            .state('app.journal', {
-                url: "/journal",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/journal.html"
-                    }
-                }
-            })
+      .state('app.journal', {
+        url: "/journal",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/journal.html"
+          }
+        }
+      })
 
-        // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/app/journal');
-    })
+      .state('app.test', {
+        url: "/test",
+        templateUrl: "templates/test.html",
+        controller: 'test'
+      })
 
-    .controller('MenuCtrl', ["$scope", "$ionicModal", "$timeout",
-        function($scope, $ionicModal, $timeout) {
-  
-            // With the new view caching in Ionic, Controllers are only called
-            // when they are recreated or on app start, instead of every page change.
-            // To listen for when this page is active (for example, to refresh data),
-            // listen for the $ionicView.enter event:      
-            //$scope.$on('$ionicView.enter', function(e) {
-            //});
-  
-            // Form data for the login modal
-            $scope.loginData = {};
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/journal');
+  })
 
-            // Create the login modal that we will use later
-            $ionicModal.fromTemplateUrl('templates/login.html', {
-                scope: $scope
-            }).then(function(modal) {
-                $scope.modal = modal;
-            });
+  .controller('MenuCtrl', ["$scope", "$ionicModal", "$timeout",
+    function ($scope, $ionicModal, $timeout) {
 
-            // Triggered in the login modal to close it
-            $scope.closeLogin = function() {
-                $scope.modal.hide();
-            };
+      // With the new view caching in Ionic, Controllers are only called
+      // when they are recreated or on app start, instead of every page change.
+      // To listen for when this page is active (for example, to refresh data),
+      // listen for the $ionicView.enter event:
+      //$scope.$on('$ionicView.enter', function(e) {
+      //});
 
-            // Open the login modal
-            $scope.login = function() {
-                $scope.modal.show();
-            };
+      // Form data for the login modal
+      $scope.loginData = {};
 
-            // Perform the login action when the user submits the login form
-            $scope.doLogin = function() {
-                console.log('Doing login', $scope.loginData);
+      // Create the login modal that we will use later
+      $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+      }).then(function (modal) {
+        $scope.modal = modal;
+      });
 
-                // Simulate a login delay. Remove this and replace with your login
-                // code if using a login system
-                $timeout(function() {
-                    $scope.closeLogin();
-                }, 1000);
-            };
-        }])
+      // Triggered in the login modal to close it
+      $scope.closeLogin = function () {
+        $scope.modal.hide();
+      };
+
+      // Open the login modal
+      $scope.login = function () {
+        $scope.modal.show();
+      };
+
+      // Perform the login action when the user submits the login form
+      $scope.doLogin = function () {
+        console.log('Doing login', $scope.loginData);
+
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function () {
+          $scope.closeLogin();
+        }, 1000);
+      };
+    }])
 ;
