4ad210a0842513ba919f5e9a107dd91c56304e53
Richard Priestley <r.priestley@comanduk.co.uk>
2015-11-16 19:40:10 +0000

    added rx tests

--- i2037-mobile/app/js/test/testController.ts
+++ i2037-mobile/app/js/test/testController.ts
@@ -9,28 +9,28 @@ testController.$inject = ['rx'];
 
 function testController(rx) {
   var clicks:Rx.Observable<any> = rx.Observable.create(function (observer) {
-      this.onClick = function (event) {
-        observer.onNext(event);
-      }
-    }.bind(this)).publish().refCount();
+    this.onClick = function (event) {
+      observer.onNext(event);
+    }
+  }.bind(this)).publish().refCount();
 
   var mousedowns:Rx.Observable<any> = rx.Observable.create(function (observer) {
     this.onMousedown = function ($event) {
       observer.onNext($event);
     }
-  }.bind(this));
+  }.bind(this)).publish().refCount();
 
   var mouseups:Rx.Observable<any> = rx.Observable.create(function (observer) {
     this.onMouseup = function ($event) {
       observer.onNext($event);
     }
-  }.bind(this));
+  }.bind(this)).publish().refCount();
 
   clicks
+    .timestamp()
+    .do((x) => { console.log("Start gesture: ", x)})
     .buffer(clicks.debounce(500))
-    .do(function(list) {
-      console.log("list: ", list);
-    })
+    .do((x) => { console.log("End gesture: ", x); })
     .map(function (list) {
       return list.length;
     })
@@ -41,4 +41,58 @@ function testController(rx) {
       console.log("Double click", e);
     })
   ;
+
+  var endgesture = mouseups.timestamp();
+
+  var gestures = mousedowns.timestamp()
+    .join(
+      endgesture,
+      function () {
+        return rx.Observable.timer(300);
+      },
+      function () {
+        return rx.Observable.timer(0);
+      },
+      function (up, down) {
+        return {mousedown: down.timestamp, mouseup: up.timestamp}
+      }
+    )
+    .do((x) => {
+      console.log('Gesture: ', x)
+    });
+
+  gestures.timeInterval()
+    .filter((x) => {
+      return x.interval < 500;
+    })
+    .subscribe(function (e) {
+      console.log("Double click", e);
+    });
+
+  //gestures.subscribe(function (e) {
+  //  console.log("subscriber1: ", e);
+  //})
+  //
+  //gestures.subscribe(function (e) {
+  //  console.log("subscriber2: ", e);
+  //})
+
+  //gestures
+  //  .join(
+  //    gestures,
+  //    function() { return rx.Observable.timer(300); },
+  //    function() { return rx.Observable.timer(0); },
+  //    function (one, two) {
+  //      return two.mouseup - one.mouseup;
+  //    }
+  //  )
+  //  .map(function (gesture) {
+  //    return gesture.length;
+  //  })
+  //  .filter(function (n) {
+  //    return n >= 2;
+  //  })
+  //  .subscribe(function (e) {
+  //    console.log("Double click", e);
+  //  })
 }
--- i2037-mobile/test/unit/test/rxjs.spec.ts
+++ i2037-mobile/test/unit/test/rxjs.spec.ts
@@ -2,43 +2,23 @@
  * Created by richard on 04/09/2015.
  */
 ///<reference path="../../../typings/tsd.d.ts" />
-///<reference path="../../../www/lib/rxjs/ts/rx.all.d.ts" />
+///<reference path="../../../www/lib/rxjs/ts/rx.lite.d.ts" />
+///<reference path="../../../www/lib/rxjs/ts/rx.time.d.ts" />
+///<reference path="../../../www/lib/rxjs/ts/rx.testing.d.ts" />
 
 describe("Reactive JS", function () {
+  var onNext = Rx.ReactiveTest.onNext;
+  var onCompleted = Rx.ReactiveTest.onCompleted;
+  var scheduler:Rx.TestScheduler;
+  var mockCommand;
 
-  xit("should be defined", function () {
-
-    expect(Rx.Observable).toBeDefined();
-
-    var obs = Rx.Observable.range(1, 3);
-    var source = obs.let(function (o) {
-      return o.concat(o);
-    });
-
-    var subscription = source.subscribe(
-      function (x) {
-        console.log(x)
-      },
-      function (err) {
-        console.log(err)
-      },
-      function () {
-        console.log()
-      }
-    )
-  })
+  beforeEach(function () {
+    mockCommand = jasmine.createSpy('mockCommand');
+    scheduler = new Rx.TestScheduler();
+  });
 
   it('should throttle', function () {
     var times = [
-      {value: 0, time: 100},
-      {value: 1, time: 600},
-      {value: 2, time: 350},
-      {value: 3, time: 900},
-      {value: 4, time: 200},
-      {value: 5, time: 150}
-    ];
-
-    var times2 = [
       {value: 0, time: 350},
       {value: 1, time: 400},
       {value: 2, time: 600},
@@ -47,7 +27,7 @@ describe("Reactive JS", function () {
       {value: 5, time: 1600},
     ];
 
-    var source = Rx.Observable.from(times2)
+    var source = Rx.Observable.from(times)
       .flatMap(function (item) {
         return Rx.Observable
           .of(item.value)
@@ -55,22 +35,67 @@ describe("Reactive JS", function () {
       })
       .debounce(300);
 
-    //var subscription = source.subscribe(
-    //  function (x) {
-    //    console.log('Next: %s', x);
-    //  },
-    //  function (err) {
-    //    console.log('Error: %s', err);
-    //  },
-    //  function () {
-    //    console.log('Done');
-    //  }
-    //)
-
     source
       .timeInterval()
-      .subscribe(function(x) {
+      .subscribe(function (x) {
         console.log("value: %s time: %s", x.value, x.interval)
       });
+  });
+
+  it('should work with the test scheduler', function () {
+    var xs = scheduler.createHotObservable(
+      onNext(250, 'a'),
+      onCompleted(260)
+    );
+
+    var result = scheduler.startWithCreate(function () {
+      return xs.map(function (x) {
+        return x;
+      });
+    });
+
+    expect(result.messages).toEqual([
+      onNext(250, 'a'),
+      onCompleted(260)
+    ]);
+
+  });
+
+  it('should concatMap', function () {
+    var xs = scheduler.createHotObservable(
+      onNext(260, 'a')
+    )
+      .do(function () {
+        console.log("do1");
+      })
+      .concatMap((x) => {
+        return mockCommand(x);
+      })
+      .publish();
+
+    mockCommand.and.callFake(function () {
+      return scheduler.createResolvedPromise(350, 'a1')
+    });
+
+    var results = [];
+
+    function log(x) {
+      console.log("value: ", x);
+    }
+
+    xs.subscribe(log);
+    xs.subscribe(log);
+    results[0] = scheduler.startWithCreate(function () {
+      return xs
+    });
+
+
+    expect(mockCommand).toHaveBeenCalled();
+    results.forEach(function (result, i) {
+      console.log("index: ", i);
+      expect(result.messages).toEqual([
+        onNext(350, 'a1'),
+      ]);
+    });
   })
-})
+});
