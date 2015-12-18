3fcba56e279f949676be16bd851f45c4b9dcf242
Richard Priestley <r.priestley@comanduk.co.uk>
2015-12-18 07:55:16 +0000

    added sass

--- i2037-client/Gruntfile.js
+++ i2037-client/Gruntfile.js
@@ -11,10 +11,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-html2js');
   grunt.loadNpmTasks('grunt-typescript');
+  grunt.loadNpmTasks('grunt-sass');
 
   grunt.config.set('dir.dist', 'build');
-  
-  grunt.registerTask('build', ['clean', 'jshint', 'html2js', 'copy', 'preprocess', 'typescript', 'concat', 'karma:unit']);
+
+  grunt.registerTask('build', ['clean', 'jshint', 'html2js', 'copy', 'preprocess', 'typescript', 'sass', 'concat', 'karma:unit']);
   grunt.registerTask('release', ['env:release', 'build', 'uglify']);
   grunt.registerTask('default', ['env:dev', 'build']);
 
@@ -30,87 +31,17 @@ module.exports = function(grunt) {
     // i2037 dir 
     dir: {
       dist: 'build',
-      bower: 'bower_components',
+      bower: 'bower_components'
     },
     clean: [ 'build/**' ],
-    env: {
-      dev: {
-          NODE_ENV : 'DEV',
-          i2037_SVC_PREFIX: '/i2037-webapp',
-          i2037_VERSION: 'dev'
-      },
-      release : {
-          NODE_ENV : 'PROD',
-          i2037_SVC_PREFIX: '',
-          i2037_VERSION: '<%= pkg.version %>'
-      }
-    },
-    copy: {
-      static: {
-        files: [
-          {expand:true, cwd: 'app', src:['img/**'], dest: '<%= dir.dist %>/'},
-          {expand:true, cwd: 'app', src:['partials/**'], dest: '<%= dir.dist %>/'},
-          {expand:true, cwd: 'app', src:['css/**'], dest: '<%= dir.dist %>/'},          
-        ]
-      },
-      bootstrap: {
-        files: [
-          {expand:true, cwd: '<%= dir.bower %>/bootstrap/dist', src:['css/**'], dest: '<%= dir.dist %>/'},
-          {expand:true, cwd: '<%= dir.bower %>/bootstrap/dist', src:['fonts/**'], dest: '<%= dir.dist %>/'},
-          {expand:true, cwd: '<%= dir.bower %>/bootstrap/dist/js', src:['**'], dest: '<%= dir.dist %>/lib/'}
-        ]
-      },
-      calendar: {
-        files: [
-          {expand:true, cwd: '<%= dir.bower %>/fullcalendar', src:['*.css'], dest: '<%= dir.dist %>/css'}
-        ]
-      }      
-    },
-    html2js: {
-      app: {
-        options: {
-          base: 'app/js'
-        },
-        src: ['app/**/*.tpl.html'],
-        dest: '<%= dir.dist %>/js/templates/<%= pkg.name %>.tpl.js',
-        module: 'templates.app'
-      }
-    },        
-    preprocess: {
-        app: {
-          options: {
-            context: {
-              name: '<%= pkg.name %>',
-              now: '<%= now %>',
-            }
-          },
-          files: [
-            {'<%= dir.dist %>/index.html': 'app/index.html' },
-            { expand:true, cwd: 'app/js', src:'**/*.js', dest: '<%= dir.dist %>/js/'},
-            { expand:true, cwd: 'app/js', src:'**/*.ts', dest: '<%= dir.dist %>/js/'}
-          ]          
-        }
-    },
-    typescript: {
-      base: {
-        src: ['<%= dir.dist %>/js/**/*.ts'],
-        dest: '<%= dir.dist %>/js/<%= pkg.name %>.ts.js',
-        options: {
-          target: 'es5', 
-          basePath: 'app/js',
-          sourceMap: true,
-          declaration: true
-        }
-      }
-    },    
     concat: {
       app: {
-        src:  ['<%= dir.dist %>/js/**/*.js'], 
+        src: ['<%= dir.dist %>/js/**/*.js'],
         dest: '<%= dir.dist %>/js/<%= pkg.name %>.js'
       },
       angular: {
-        src:[ 
-          '<%= dir.bower %>/angular/angular.js', 
+        src: [
+          '<%= dir.bower %>/angular/angular.js',
           '<%= dir.bower %>/angular-route/angular-route.js',
           '<%= dir.bower %>/angular-resource/angular-resource.js',
           '<%= dir.bower %>/angular-bootstrap/ui-bootstrap-tpls.js',
@@ -126,14 +57,111 @@ module.exports = function(grunt) {
         dest: '<%= dir.dist %>/lib/jquery.js'
       },
       depsjs: {
-        src: [ 
-          '<%= dir.bower %>/spinjs/spin.js', 
+        src: [
+          '<%= dir.bower %>/spinjs/spin.js',
           '<%= dir.bower %>/fullcalendar/fullcalendar.js',
           '<%= dir.bower %>/d3/d3.js',
           '<%= dir.bower %>/autofill-event/src/autofill-event.js'
         ],
         dest: '<%= dir.dist %>/lib/deps.js'
       },
+    },
+    copy: {
+      static: {
+        files: [
+          {expand: true, cwd: 'app', src: ['img/**'], dest: '<%= dir.dist %>/'},
+          {expand: true, cwd: 'app', src: ['partials/**'], dest: '<%= dir.dist %>/'},
+          {expand: true, cwd: 'app', src: ['css/**'], dest: '<%= dir.dist %>/'},
+        ]
+      },
+      bootstrap: {
+        files: [
+          {expand: true, cwd: '<%= dir.bower %>/bootstrap/dist', src: ['css/**'], dest: '<%= dir.dist %>/'},
+          {expand: true, cwd: '<%= dir.bower %>/bootstrap/dist', src: ['fonts/**'], dest: '<%= dir.dist %>/'},
+          {expand: true, cwd: '<%= dir.bower %>/bootstrap/dist/js', src: ['**'], dest: '<%= dir.dist %>/lib/'}
+        ]
+      },
+      calendar: {
+        files: [
+          {expand: true, cwd: '<%= dir.bower %>/fullcalendar', src: ['*.css'], dest: '<%= dir.dist %>/css'}
+        ]
+      }
+    },
+    env: {
+      dev: {
+          NODE_ENV : 'DEV',
+          i2037_SVC_PREFIX: '/i2037-webapp',
+          i2037_VERSION: 'dev'
+      },
+      release : {
+          NODE_ENV : 'PROD',
+          i2037_SVC_PREFIX: '',
+          i2037_VERSION: '<%= pkg.version %>'
+      }
+    },
+    html2js: {
+      app: {
+        options: {
+          base: 'app/js'
+        },
+        src: ['app/**/*.tpl.html'],
+        dest: '<%= dir.dist %>/js/templates/<%= pkg.name %>.tpl.js',
+        module: 'templates.app'
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'app/**/*.js'],
+      options: {
+        debug: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
+      }
+    },
+    karma: {
+      unit: {configFile: 'config/karma.conf.js', singleRun: true, autoWatch: false},
+      watch: {configFile: 'config/karma.conf.js', singleRun: false, autoWatch: true}
+    },
+    preprocess: {
+        app: {
+          options: {
+            context: {
+              name: '<%= pkg.name %>',
+              now: '<%= now %>',
+            }
+          },
+          files: [
+            {'<%= dir.dist %>/index.html': 'app/index.html' },
+            { expand:true, cwd: 'app/js', src:'**/*.js', dest: '<%= dir.dist %>/js/'},
+            { expand:true, cwd: 'app/js', src:'**/*.ts', dest: '<%= dir.dist %>/js/'}
+          ]          
+        }
+    },
+    sass: {
+      dist: {
+        files: {
+          '<%= dir.dist %>/css/app.css': 'app/css/app.scss'
+        }
+      }
+    },
+    typescript: {
+      base: {
+        src: ['<%= dir.dist %>/js/**/*.ts'],
+        dest: '<%= dir.dist %>/js/<%= pkg.name %>.ts.js',
+        options: {
+          target: 'es5', 
+          basePath: 'app/js',
+          sourceMap: true,
+          declaration: true
+        }
+      }
     },
     uglify: {
       options: {
@@ -156,28 +184,8 @@ module.exports = function(grunt) {
         dest: '<%= dir.dist %>/lib/deps.min.js'
       }          
     },
-    karma: {
-      unit: { configFile: 'config/karma.conf.js', singleRun: true, autoWatch: false },
-      watch: { configFile: 'config/karma.conf.js', singleRun: false, autoWatch: true }
-    },    
-    jshint: {
-      files: ['Gruntfile.js', 'app/**/*.js'],
-      options:{
-        debug:true,
-        curly:true,
-        eqeqeq:true,
-        immed:true,
-        latedef:true,
-        newcap:true,
-        noarg:true,
-        sub:true,
-        boss:true,
-        eqnull:true,
-        globals:{}
-      }      
-    },
     watch: {
-      files: ['Gruntfile.js', 'app/**/*.ts', 'test/unit/**/*.js', 'app/**/*.html', 'app/**/*.css'],
+      files: ['Gruntfile.js', 'app/**/*.ts', 'test/unit/**/*.js', 'app/**/*.html', 'app/**/*.scss'],
       tasks: ['build']
     }
   });
