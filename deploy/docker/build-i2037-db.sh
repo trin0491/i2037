bed41e25170fbadecfa04403543e0500b125c3ad
Richard Priestley <r.priestley@comanduk.co.uk>
2017-01-15 21:06:11 +0000

    added kubernetes for webapp and db

--- deploy/docker/build-i2037-db.sh
+++ deploy/docker/build-i2037-db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t i2037-db i2037-db
+docker build -t i2037-db:1.0.0 i2037-db
---
+++ deploy/kubernetes/mysql.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+kubectl run -it --rm --image=mysql:5.6 mysql-client -- mysql -h i2037-db -ppassword
