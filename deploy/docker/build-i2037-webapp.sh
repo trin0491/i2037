51cf530a851618557e661f9bb308fcb32b1031f4
Richard Priestley <r.priestley@comanduk.co.uk>
2017-01-19 19:14:23 +0000

    fixed war with client libs

--- deploy/docker/build-i2037-webapp.sh
+++ deploy/docker/build-i2037-webapp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp ../../i2037-webapp/build/libs/i2037-webapp-1.0.0-SNAPSHOT.war ./i2037-webapp
-docker build -t i2037-webapp:1.0.0 i2037-webapp
+docker build -t i2037-webapp:1.0.1 i2037-webapp
