b1634d85e95927fde787e5bdaa605a3c426ac096
Richard Priestley <r.priestley@comanduk.co.uk>
2017-02-05 13:58:30 +0000

    moved from travis to jenkins

--- i2037-client/Gruntfile.js
+++ i2037-client/Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
 
   var karmaConfig = function(configFile, customOptions) {
     var options = { configFile: configFile, keepalive: true };
-    var travisOptions = process.env.TRAVIS && { browsers: ['Firefox'], reporters: 'dots' };
+    var travisOptions = process.env.JENKINS_HOME && { browsers: ['Firefox'], reporters: 'dots' };
     return grunt.util._.extend(options, customOptions, travisOptions);
   };
 
