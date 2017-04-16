48ddbf61cd4e4f60a831e05ad84def94a303ccd6
Richard Priestley <r.priestley@comanduk.co.uk>
2017-04-16 22:03:43 +0100

    made passwords generic

--- deploy/kubernetes/create-secrets.sh
+++ deploy/kubernetes/create-secrets.sh
@@ -4,5 +4,5 @@ then
   echo "usage: $0 <root_pass> <db_pass>"
   exit 1
 fi
-kubectl create secret generic i2037-db-root-pass --from-literal=password=qNee4765
-kubectl create secret generic i2037-db-pass --from-literal=password=richard
+kubectl create secret generic i2037-db-root-pass --from-literal=password=$1
+kubectl create secret generic i2037-db-pass --from-literal=password=$2
--- deploy/kubernetes/mysql.sh
+++ deploy/kubernetes/mysql.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-kubectl run -it --rm --image=mysql:5.6 mysql-client -- mysql -h i2037-db -pqNee4765
+if [ "$#" -lt 1 ]
+then
+  echo "usage: $0 <root_pass>"
+  exit 1
+fi
+kubectl run -it --rm --image=mysql:5.6 mysql-client -- mysql -h i2037-db -p$1
