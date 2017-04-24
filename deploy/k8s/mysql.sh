ba4247e84f14b411d2fee73e6e7ac342801d8834
Richard Priestley <r.priestley@comanduk.co.uk>
2017-04-24 22:14:26 +0100

    added persistent volume

--- deploy/k8s/mysql.sh
+++ deploy/k8s/mysql.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [ "$#" -lt 1 ]
-then
-  echo "usage: $0 <root_pass>"
-  exit 1
-fi
-kubectl run -it --rm --image=mysql:5.6 mysql-client -- mysql -h i2037-db -p$1
+kubectl run -it --rm --image=mysql:5.6 mysql-client -- mysql -h i2037-db -p
