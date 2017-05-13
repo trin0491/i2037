4773baf4149bf02ee0fb9d5a43fd14216de7a262
Richard Priestley <r.priestley@comanduk.co.uk>
2017-05-13 14:03:39 +0100

    cleaned up build scripts

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 cd i2037-client
-npm install
-grunt release
+./build.sh
 cd ../i2037-webapp
-./gradlew build
-cd ../deploy/docker
-./build-i2037-webapp.sh
-./build-i2037-db.sh
+./build.sh
+cd ../deploy
+./build.sh
---
+++ deploy/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd docker
+./build-i2037-webapp.sh
+./build-i2037-db.sh
