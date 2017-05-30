8ac947f53d4857ce3f556f51d0858f47e8df0878
Richard Priestley <r.priestley@comanduk.co.uk>
2017-05-30 22:19:40 +0100

    added tls to load balancer

--- i2037-deploy/scripts/create-secrets.sh
+++ i2037-deploy/scripts/create-secrets.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 if [ "$#" -lt 3 ]
 then
-  echo "usage: $0 <root_pass> <db_pass> <secret-file>"
+  echo "usage: $0 <root_pass> <db_pass> <env>"
   exit 1
 fi
 kubectl create secret generic i2037-db-root-pass --from-literal=password=$1
 kubectl create secret generic i2037-db-user-pass --from-literal=password=$2 --from-literal=username=richard
-kubectl create secret generic i2037-webapp --from-file=$3
+kubectl create secret generic i2037-webapp --from-file=../env/$env/secret.properties
+kubectl create secret tls i2037-cert --cert ../env/$env/tls.crt --key ../env/$env/tls.key
